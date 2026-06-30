469ad7610dc6b25388bbc6d73b592f4f94bfbbf4
TinaaiZhuzhu <christinaxiting@gmail.com>
2026-06-30 22:32:07 +1000

    Delete image

--- sql/02_data_validation.sql
+++ sql/02_data_validation.sql
@@ -1,5 +1,7 @@
--- Check row count
-
+--   Check table structure
+SELECT *
+FROM consent_journey
+Limit 10;
 
 -- Check row count
 SELECT COUNT(*)
@@ -14,4 +16,21 @@ SELECT consent_status,
        COUNT(*)
 FROM consent_journey
 GROUP BY consent_status;
+
+-- Check error reason for CBA
+SELECT error_reason, COUNT(*) AS journey_count
+FROM consent_journey
+WHERE data_holder = 'CBA' AND consent_status != 'Completed'
+GROUP BY error_reason
+ORDER BY journey_count DESC;
+
+--Business Question:
+-- For CBA, identify the most common reasons why consent journeys were abandoned.
+
+SELECT error_reason, COUNT(*) AS journey_count,
+Round(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (), 2) AS percentage
+FROM consent_journey
+WHERE data_holder = 'CBA' AND consent_status = 'Abandoned'
+GROUP BY error_reason
+ORDER BY journey_count DESC;
 
---
+++ sql/03.view.sql
@@ -0,0 +1,19 @@
+--Create a view to store the results of the above query for future reference
+CREATE OR REPLACE VIEW vw_cba_abandoned_errors AS
+SELECT error_reason, COUNT(*) AS journey_count,
+       Round(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (), 2) AS percentage
+FROM consent_journey
+WHERE data_holder = 'CBA' AND consent_status = 'Abandoned'
+GROUP BY error_reason
+ORDER BY journey_count DESC;
+
+
+CREATE OR REPLACE VIEW vw_consent_status_summary AS
+SELECT consent_status,
+       COUNT(*) AS journey_count
+FROM consent_journey
+GROUP BY consent_status
+ORDER BY journey_count DESC;
+
+SELECT *
+FROM vw_consent_status_summary
