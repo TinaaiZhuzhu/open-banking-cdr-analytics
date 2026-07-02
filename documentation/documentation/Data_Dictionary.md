92108898762802f66331eb46c1c2e55b9f320906
TinaaiZhuzhu <christinaxiting@gmail.com>
2026-07-02 22:38:41 +1000

    Build Executive Dashboard v1 with KPI cards and failure analysis

--- documentation/documentation/Data_Dictionary.md
+++ documentation/documentation/Data_Dictionary.md
@@ -7,3 +7,12 @@ This document describes the fields used in the simulated Open Banking CDR consen
 The dataset is designed for SQL analysis, Power BI dashboards and business analysis portfolio projects.
 
 ---
+## Dashboard Views
+
+### vw_cba_abandoned_errors
+
+Purpose:
+Provides the distribution of abandonment reasons for CBA consent journeys.
+
+Used in:
+Executive Dashboard - Top Consent Failure Reasons
--- sql/03.view.sql
+++ sql/03.view.sql
@@ -1,12 +1,14 @@
 --Create a view to store the results of the above query for future reference
 CREATE OR REPLACE VIEW vw_cba_abandoned_errors AS
 SELECT error_reason, COUNT(*) AS journey_count,
-       Round(COUNT(*) * 100.0 / SUM(COUNT(*)) OVER (), 2) AS percentage
+       Round(COUNT(*) / SUM(COUNT(*)) OVER (), 2) AS percentage
 FROM consent_journey
 WHERE data_holder = 'CBA' AND consent_status = 'Abandoned'
 GROUP BY error_reason
 ORDER BY journey_count DESC;
 
+SELECT *
+FROM vw_cba_abandoned_errors;
 
 CREATE OR REPLACE VIEW vw_consent_status_summary AS
 SELECT consent_status,
